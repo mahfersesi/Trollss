606652a13778a68a5a2b6a8733236f36b07d67c2
mahfersesi <131968124+mahfersesi@users.noreply.github.com>
2024-07-22 09:04:38 -0300

    22/07
    começando o projeto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StockInspection;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Cliente.cs
@@ -0,0 +1,17 @@
+Clente.cs
+Public class Cliente
+{
+    string nome;
+    int64 id;
+
+Public void SetNome(string nome)
+{
+    thisnome= nome;
+}
+Public string GetNome()
+{
+    return nome;
+}
+
+
+}

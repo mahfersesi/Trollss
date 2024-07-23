c697f5b4486192fd0ad15616f217e1ec707ea7db
maria fernanda <mariafernandalibano345@gmail.com>
2024-07-23 09:05:38 -0300

    continuando classes

---
+++ MateriaP.cs
@@ -0,0 +1,28 @@
+MateriaP.cs
+Public class MateriaP : Cliente
+{
+    string nomeMP;
+    string UnidadeDeMedida;
+    string NomeCliente;
+
+
+Public void SetNomeCliente(string NomeCliente)
+{
+    thisNomeCliente= NomeCliente;
+}
+Public string GetNomeCliente()
+{
+    return NomeCliente;
+}
+
+Public void SetUnidadeDeMedida(string UnidadeDeMedida)
+{
+    thisUnidadeDeMedida= UnidadeDeMedida;
+}
+Public string GetUnidadeDeMedida()
+{
+    return UnidadeDeMedida;
+}
+
+
+}
